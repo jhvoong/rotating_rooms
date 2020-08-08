c0f22b610355e9fc7bd13251961b697a96bfcbe1
John <jhvoong@ucdavis.edu>
2020-08-08 10:07:10 -0700

    Added rotations

--- index.js
+++ index.js
@@ -1,13 +1,50 @@
-let level = 10
-let size = 1 / level * 100
-let square;
+let level = 3
+let size = 1 / (level * 2) * 100
+let square
+let squares = {}
 let grid = document.querySelector("#grid")
 
+class Square {
+  constructor(i, j) {
+    this.content = document.createElement("div")
+    this.content.id = `${i}-${j}`
+
+    // add styling
+    this.content.classList.add("square")
+    // this.content.style.flexBasis = `${size}%`
+    this.content.style.width = `${size}vw`
+    this.content.style.height = `${size}vw`
+
+    // add rotation functionality
+    this.content.addEventListener("click", rotate)
+  }
+}
+
+function rotate(event) {
+  const [coordX, coordY] = this.id.split("-")
+  const info = squares[coordX][coordY]
+  const nextDeg = info.degrees + 90
+  info.square.style.webkitTransform = `rotate(${nextDeg}deg)`;
+  info.square.style.transform = `rotate(${nextDeg}deg)`;
+  info.square.style.MozTransform = `rotate(${nextDeg}deg)`;
+  info.square.style.msTransform = `rotate(${nextDeg}deg)`;
+  info.square.style.OTransform = `rotate(${nextDeg}deg)`;
+  squares[coordX][coordY].degrees = nextDeg
+}
+
 function init() {
-  for (let i = 0; i < level ** 2; i++) {
-    square = document.createElement("div")
-    square.classList.add("square")
-    square.style.flexBasis = `${size}%`
-    grid.appendChild(square)
+  for (let i = 0; i < level; i++) {
+    squares[i] = {}
+    for (let j = 0; j < level; j++) {
+      // create element
+      square = new Square(i, j).content
+      squares[i][j] =  {
+        square: square,
+        degrees: 0
+      }
+
+      // add to html
+      grid.appendChild(square)
+    }
   }
 }
